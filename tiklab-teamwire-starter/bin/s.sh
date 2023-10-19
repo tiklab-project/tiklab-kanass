7cf57f9373261c22ec85662c976d7c7a67aa75e6
袁婕轩 <yuanjiexuan@darthcloud.com>
2023-10-19 17:22:24 +0800

    修改bug

--- tiklab-teamwire-starter/bin/s.sh
+++
@@ -1,8 +0,0 @@
-PGPASSWORD=darth2020 /Library/PostgreSQL/15/bin/pg_dump -U postgres -h 172.10.1.10 -p 5432 -d tiklab_teamwire -n public>/Users/yuanjiexuan/tiklab/teamwire/backup/tiklab_teamwire.sql
-
-#create db
-
-
-
-
-
