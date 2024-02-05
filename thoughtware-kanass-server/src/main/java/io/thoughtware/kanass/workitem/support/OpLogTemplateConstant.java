e492834aaa6d005b0cbbede6dfb1876c54874ce7
袁婕轩 <yuanjiexuan@darthcloud.com>
2024-02-05 19:34:11 +0800

    修改迭代, 版本与事项的关系

--- thoughtware-kanass-server/src/main/java/io/thoughtware/kanass/workitem/support/OpLogTemplateConstant.java
+++ thoughtware-kanass-server/src/main/java/io/thoughtware/kanass/workitem/support/OpLogTemplateConstant.java
@@ -6,8 +6,6 @@ package io.thoughtware.kanass.workitem.support;
 public class  OpLogTemplateConstant {
 
 
-
-
     public static final String KANASS_LOGTYPE_WORKITEMUPDATE = "1bba4e45931f";
 
     public static final String KANASS_LOGTYPE_WORKITEMADD = "a1f2cb478fad";
