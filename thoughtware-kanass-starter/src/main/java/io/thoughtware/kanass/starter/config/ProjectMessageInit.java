3f69402e8d014529bd99ff93c606194d638e6426
袁婕轩 <yuanjiexuan@darthcloud.com>
2024-05-13 21:08:31 +0800

    修改bug

--- thoughtware-kanass-starter/src/main/java/io/thoughtware/kanass/starter/config/ProjectMessageInit.java
+++ thoughtware-kanass-starter/src/main/java/io/thoughtware/kanass/starter/config/ProjectMessageInit.java
@@ -17,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
+//@Component
 public class ProjectMessageInit implements ApplicationRunner {
 
     @Autowired
