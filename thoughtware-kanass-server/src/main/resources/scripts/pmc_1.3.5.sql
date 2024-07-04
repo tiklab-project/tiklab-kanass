8eed88addea29d6a01103b584d98c1f2658aa616
袁婕轩 <yuanjiexuan@darthcloud.com>
2024-07-04 18:24:03 +0800

    更新项目流程数据

--- thoughtware-kanass-server/src/main/resources/scripts/pmc_1.3.5.sql
+++ thoughtware-kanass-server/src/main/resources/scripts/pmc_1.3.5.sql
@@ -16,6 +16,3 @@ DELETE FROM pcs_foc_form_field;
 DELETE FROM pcs_foc_field;
 DELETE FROM pcs_foc_form_relation;
 
-INSERT INTO pcs_flc_flow_relation (id, model_id, model_name, model_type, flow_id, bgroup) VALUES ('5ad56f59e2ae', '778222e0', '任务', 'workType', '4d040c6d', 'kanass');
-INSERT INTO pcs_flc_flow_relation (id, model_id, model_name, model_type, flow_id, bgroup) VALUES ('84a2f6124d31', '98121701', '缺陷', 'workType', '3d879830', 'kanass');
-INSERT INTO pcs_flc_flow_relation (id, model_id, model_name, model_type, flow_id, bgroup) VALUES ('5a142fd02abc', '7055ebc6', '需求', 'workType', 'a96cf9c9', 'kanass');
