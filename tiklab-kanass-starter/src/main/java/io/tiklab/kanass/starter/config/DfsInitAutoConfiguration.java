736fef9f5cad02f6b473aa5b1626ebcd3cc1c9fc
袁婕轩 <yuanjiexuan@darthcloud.com>
2025-01-08 09:15:28 +0800

    dfs 更改

--- tiklab-kanass-starter/src/main/java/io/tiklab/kanass/starter/config/DfsInitAutoConfiguration.java
+++ tiklab-kanass-starter/src/main/java/io/tiklab/kanass/starter/config/DfsInitAutoConfiguration.java
@@ -2,7 +2,8 @@ package io.tiklab.kanass.starter.config;
 
 
 
-import io.tiklab.dfs.init.initdata.config.model.DfsConfig;
+
+import io.tiklab.dfs.init.config.model.DfsConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
