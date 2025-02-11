7f89c4ae5aa35f963d221af047411f0d641333fc
袁婕轩 <yuanjiexuan@darthcloud.com>
2025-02-11 13:46:55 +0800

    static  前端ce版 去除cloud ee组件

--- tiklab-kanass-server/src/main/resources/scripts/pmc_1.4.6.sql
+++ tiklab-kanass-server/src/main/resources/scripts/pmc_1.4.6.sql
@@ -1,14 +1,14 @@
 
-INSERT INTO "public"."pcs_mec_message_template" ("id", "msg_type_id", "msg_send_type_id", "title", "content", "link", "bgroup", "link_params") VALUES ('maildfdsdf', 'KANASS_MESSAGETYPE_SPRINTUPDATE', 'email', '更新迭代状态', '📌 迭代状态更新 | <font color="#409EFF">${sprintName}</font>    <br>
+INSERT INTO "pcs_mec_message_template" ("id", "msg_type_id", "msg_send_type_id", "title", "content", "link", "bgroup", "link_params") VALUES ('maildfdsdf', 'KANASS_MESSAGETYPE_SPRINTUPDATE', 'email', '更新迭代状态', '📌 迭代状态更新 | <font color="#409EFF">${sprintName}</font>    <br>
 ▫️ 操作人：${createUser.nickname}   <br>
 ▫️ 状态变更：<font color="#909399">${oldValue}</font> ➔ <font color="#67C23A">${newValue}</font>', '/${projectId}/sprintdetail/${sprintId}/workTable', 'kanass', NULL);
 
 
-INSERT INTO "public"."pcs_mec_message_template" ("id", "msg_type_id", "msg_send_type_id", "title", "content", "link", "bgroup", "link_params") VALUES ('maildfsdggh', 'KANASS_MESSAGETYPE_SPRINTCREATE', 'email', '添加迭代', '✨ 添加迭代 | <font color="#409EFF">${sprintName}</font>
+INSERT INTO "pcs_mec_message_template" ("id", "msg_type_id", "msg_send_type_id", "title", "content", "link", "bgroup", "link_params") VALUES ('maildfsdggh', 'KANASS_MESSAGETYPE_SPRINTCREATE', 'email', '添加迭代', '✨ 添加迭代 | <font color="#409EFF">${sprintName}</font>
 ▫️ 创建人：${createUser.nickname}', '/${projectId}/sprintdetail/${sprintId}/workTable', 'kanass', NULL);
 
 
-INSERT INTO "public"."pcs_mec_message_template" ("id", "msg_type_id", "msg_send_type_id", "title", "content", "link", "bgroup", "link_params") VALUES ('emailworkitemstatus', 'KANASS_MESSAGETYPE_UPDATESTATUS', 'email', '事项状态变更', '📌 更新事项 | <font color="info">${workItemTitle}</font> 状态 <br>
+INSERT INTO "pcs_mec_message_template" ("id", "msg_type_id", "msg_send_type_id", "title", "content", "link", "bgroup", "link_params") VALUES ('emailworkitemstatus', 'KANASS_MESSAGETYPE_UPDATESTATUS', 'email', '事项状态变更', '📌 更新事项 | <font color="info">${workItemTitle}</font> 状态 <br>
 ▫️ 操作人：${createUser.nickname} <br>
 ▫️ 状态变更：<font color="#909399">${oldValue}</font> ➔ <font color="#67C23A">${newValue}</font>
 ', '/projectDetail/${projectId}/work/${workItemId}', 'kanass', NULL);
